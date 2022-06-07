5ee2b3cdb42f43e8a3fe7aeb85ca88ec1a25d2ae
AndreiSon88 <avm.tehniks@yandex.ru>
2022-06-07 18:22:23 +0300

    Signed-off-by: AndreiSon88 <avm.tehniks@yandex.ru>

--- 19.2.3  second_test/app/calculator.py
+++ 19.2.3  second_test/app/calculator.py
@@ -1,11 +1,11 @@
 
 
 class Calculator:
-    def multiply(self,x , y):
-        return x*y
-    def division(self,x ,y):
+    def multiply(self, x, y):
+        return x * y
+    def division(self, x, y):
         return x / y
-    def substraction(self,x,y):
+    def substraction(self, x, y):
         return x - y
-    def adding(self, x,y):
+    def adding(self, x, y):
         return x + y
